e126226b6df85d7db0d635cb1c019c9da09fc213
CodeBreakerMG <mmoran002@protonmail.com>
2022-04-20 09:22:57 -0500

    PRIMER PUSH
    PUSH

---
+++ g4algoritmos/src/test/java/pe/edu/pucp/g4algoritmos/AppTest.java
@@ -0,0 +1,20 @@
+package pe.edu.pucp.g4algoritmos;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
