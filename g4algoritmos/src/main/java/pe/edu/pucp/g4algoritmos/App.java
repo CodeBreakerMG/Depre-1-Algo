0a88f0b1e4cc37f9a012521c276395275df6c5be
AngelG123 <a20175488@pucp.edu.pe>
2022-05-02 20:39:08 -0500

    a

--- g4algoritmos/src/main/java/pe/edu/pucp/g4algoritmos/App.java
+++ g4algoritmos/src/main/java/pe/edu/pucp/g4algoritmos/App.java
@@ -3,6 +3,9 @@ package pe.edu.pucp.g4algoritmos;
 import pe.edu.pucp.g4algoritmos.solucion1.*;
 import pe.edu.pucp.g4algoritmos.pso.PSOMain;
 
+import java.io.File;
+
+import pe.edu.pucp.g4algoritmos.model.Mapa;
 /*
 
 DEPRE-1-ALGORITMOS
@@ -20,6 +23,17 @@ public class App
         PrimeraSolucion solucion = new PrimeraSolucion();
 
         //Primero, cargar la data al maestro MAPA
+        final String sep = File.separator;
+
+        Mapa.cargarAlmacenesYOficinas(System.getProperty("user.dir")+sep+"data"+sep+"inf226.oficinas.txt");
+        Mapa.cargarTramos(System.getProperty("user.dir")+sep+"data"+sep+"inf226.tramos.v.2.0.txt");
+        Mapa.cargarPedidos(System.getProperty("user.dir")+sep+"data"+sep+"inf226.ventas202203.txt",
+                           System.getProperty("user.dir")+sep+"data"+sep+"inf226.ventas202204.txt",
+                           System.getProperty("user.dir")+sep+"data"+sep+"inf226.ventas202205.txt");
+        
+        System.out.println(String.format("Cantidad de oficinas: %4d", Mapa.listaOficinas.size() + Mapa.listaAlmacenes.size()));
+        System.out.println(String.format("Cantidad de tramos:   %4d", Mapa.listaTramos.size()));
+        System.out.println(String.format("Cantidad de pedidos:  %4d", Mapa.listaPedidos.size()));
 
         //Función que llama al PSO:
         //PSOMain.Main();
